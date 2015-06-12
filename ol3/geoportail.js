e2f6181479f7f91e402cabf76ddbe8a6a41e2904
Viglino <jean-marc.viglino@ign.fr>
2015-06-12 17:12:17 +0200

    [ADD] OL3 Geoportail map and layers to handle API key.

--- ol3/geoportail.js
+++ ol3/geoportail.js
@@ -1,13 +1,13 @@
 ﻿/**
-* @constructor IGN's Geoportail WMTS layer definition
+* @constructor IGN's Geoportail WMTS source definition
 * @extends {ol.source.WMTS}
-* @param {String=} key API key.
 * @param {String=} layer Layer name.
-* @param {olx.source.OSMOptions=} options WMTS options.
+* @param {olx.source.OSMOptions=} options WMTS options + key: apiKey
 * @todo 
 */
-ol.source.Geoportail = function(key, layer, options)
+ol.source.Geoportail = function(layer, options)
 {	if (!options) options={};
+
 	var matrixIds = new Array();
 	var resolutions = new Array();//[156543.03392804103,78271.5169640205,39135.75848201024,19567.879241005125,9783.939620502562,4891.969810251281,2445.9849051256406,1222.9924525628203,611.4962262814101,305.74811314070485,152.87405657035254,76.43702828517625,38.218514142588134,19.109257071294063,9.554628535647034,4.777314267823517,2.3886571339117584,1.1943285669558792,0.5971642834779396,0.29858214173896974,0.14929107086948493,0.07464553543474241];
 	var size = ol.extent.getWidth(ol.proj.get('EPSG:3857').getExtent()) / 256;
@@ -24,7 +24,7 @@ ol.source.Geoportail = function(key, layer, options)
 	var attr = [ ol.source.Geoportail.prototype.attribution ];
 	if (options.attributions) attr.push(options.attributions);
 	ol.source.WMTS.call (this, 
-		{	url: "http://wxs.ign.fr/" + key + "/wmts",
+		{	url: "http://wxs.ign.fr/" + options.key + "/wmts",
 			layer: layer,
 			matrixSet: "PM",
 			format: options.format ? options.format:"image/jpeg",
@@ -32,11 +32,84 @@ ol.source.Geoportail = function(key, layer, options)
 			tileGrid: tg,
 			style: options.style ? options.style:"normal",
 			attributions: attr
-		});	
+		});
+	// Save function to change apiKey
+	this._urlFunction = this.getTileUrlFunction();
 };
 ol.inherits (ol.source.Geoportail, ol.source.WMTS);
+
+
+
+ol.source.Geoportail.prototype.getGPPKey = function()
+{	var url = this.getUrls()[0];
+	return url.replace (/http:\/\/wxs.ign.fr\/(.*)\/.*/,"$1");
+}
+ol.source.Geoportail.prototype.setGPPKey = function(key)
+{	var url = this.getUrls();
+	url[0] = url[0].replace (/(http:\/\/wxs.ign.fr\/)(.*)(\/.*)/, "$1"+key+"$3");
+	this.setTileUrlFunction ( function()
+	{	var url = this._urlFunction.apply(this, arguments);
+		return url.replace (/(http:\/\/wxs.ign.fr\/)(.*)(\/.*)/, "$1"+key+"$3");
+	});
+}
 
 // Attribution standard
 ol.source.Geoportail.prototype.attribution = new ol.Attribution
 ({	html: '<a href="http://www.geoportail.gouv.fr/">Géoportail</a> &copy; <a href="http://www.ign.fr/">IGN-France</a>'
 });
+
+
+/**
+* @constructor IGN's Geoportail WMTS layer definition
+* @extends {ol.layer.Tile}
+* @param {options=} layer: Layer name, key: APIKey.
+* @param {olx.source.OSMOptions=} options WMTS options if not defined default are used
+* @todo 
+*/
+ol.layer.Geoportail = function(layer, options, tileoptions)
+{	if (!options) options={};
+	if (!tileoptions) tileoptions={};
+	options.layer = layer;
+
+	var capabilities = window.geoportailConfig ? geoportailConfig.capabilities[options.key] || geoportailConfig.capabilities["default"] : {};
+	capabilities = capabilities[options.layer] || {};
+
+	for (var i in capabilities) if (typeof	tileoptions[i]== "undefined") tileoptions[i] = capabilities[i];
+
+	if (!tileoptions.key) tileoptions.key = options.key;
+	options.source = new ol.source.Geoportail(options.layer, tileoptions);
+	options.name = capabilities.title;
+	options.desc = capabilities.desc;
+
+	ol.layer.Tile.call (this, options);	
+};
+ol.inherits (ol.layer.Geoportail, ol.layer.Tile);
+
+
+/**
+* @constructor IGN's Geoportail Map definition
+* @extends {ol.Map}
+* @param {ol.Map.options=} add the API key to the map options (key)
+* @todo 
+*/
+ol.Map.Geoportail = function(options)
+{	// constructor
+	ol.Map.apply (this, arguments);
+	
+	// Set API key to the Geoportail layers
+	if (!options) options={};
+	this.gppKey = options.key;
+	if (this.gppKey && options.layers)
+	{	for (var i in options.layers)
+			if (options.layers[i].getSource().setGPPKey) options.layers[i].getSource().setGPPKey(this.gppKey);
+	}
+}
+ol.inherits (ol.Map.Geoportail, ol.Map);
+
+/**
+*	Set the API key to Geoportail layers when added
+*/
+ol.Map.Geoportail.prototype.addLayer = function(layer)
+{	if (this.gppKey && layer.getSource().setGPPKey) layer.getSource().setGPPKey(this.gppKey);
+	return ol.Map.prototype.addLayer.apply (this, arguments);
+}
