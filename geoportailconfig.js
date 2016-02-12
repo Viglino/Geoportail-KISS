4951d3e6e2aebbc25a3cd3aea318941061d13ce4
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2016-02-12 08:11:51 +0100

    [ADD] check for all resources

--- geoportailconfig.js
+++ geoportailconfig.js
@@ -1574,7 +1574,7 @@ var geoportailConfig =
 	key			{String} the API key
 	success		{function} a collaback function 
 */
-geoportailConfig.jqautoconf = function(key, success)
+geoportailConfig.jqautoconf = function(key, success, all)
 {	var geopresolutions = [156543.03390625,78271.516953125,39135.7584765625,19567.87923828125,9783.939619140625,4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677,0.14929107084870338];
 	function getZoom(res)
 	{	res = Number(res);
@@ -1598,7 +1598,7 @@ geoportailConfig.jqautoconf = function(key, success)
 			for (var i=0; i<layers.length; i++)
 			{	var l = $(layers[i]);
 				if (!/WMTS/.test(l.find("Server").attr("service"))) continue;
-				if (!/geoportail\/wmts/.test(l.find("OnlineResource").attr("href"))) continue;
+				if (!all && !/geoportail\/wmts/.test(l.find("OnlineResource").attr("href"))) continue;
 				var service = js[l.find("Name:first").text()] = {};
 				service.title = l.find("Title:first").text();
 				service.order = l.find("Extension Layer").attr("order");
